20e80bfb7dbf533c291bd8d9768e0b4f6f221c1b
Elena Piaggio <helenamercury@gmail.com>
2019-07-02 13:38:56 +0200

    ADD project structure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// document.onload(function () {
+    // for control the Canvas
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(10,10,15,15);
+// });
+
+
