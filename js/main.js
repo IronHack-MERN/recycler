b99424f1dd33f0ee52ed79ca2c2bee2b35d5be2f
Elena Piaggio <helenamercury@gmail.com>
2019-07-14 22:35:50 +0200

    ADD start button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-document.onload = (function() {
+// document.onload = (function() {
+//     const canvas = document.getElementById('canvas');
+//     const ctx = canvas.getContext('2d');
+    
+//     let game = new Game({
+//         ctx: ctx,
+//         canvas: canvas
+//     });
+
+//     game._start();
+
+// })();
+
+function start(){
+
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    
+
+    const container = document.querySelector("#container");
+    const btnStart = document.querySelector("#btnStart");
+
     let game = new Game({
         ctx: ctx,
         canvas: canvas
@@ -9,4 +26,9 @@ document.onload = (function() {
 
     game._start();
 
-})();
+
+    console.log("empezando");
+}
+
+
+
